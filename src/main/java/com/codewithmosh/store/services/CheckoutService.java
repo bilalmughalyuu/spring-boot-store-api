eeac33223e27237ea756667b7ab55a9e16ff8171
Bilal <bilal@atlastechfz.com>
2026-09-26 18:31:39 +0400

    Added the Stripe Webhook for Payment Processing

--- src/main/java/com/codewithmosh/store/services/CheckoutService.java
+++ src/main/java/com/codewithmosh/store/services/CheckoutService.java
@@ -2,17 +2,23 @@ package com.codewithmosh.store.services;
 
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
-import com.codewithmosh.store.dtos.ErrorDto;
 import com.codewithmosh.store.entities.Order;
+import com.codewithmosh.store.entities.PaymentStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
+import com.codewithmosh.store.exceptions.PaymentException;
 import com.codewithmosh.store.repositories.CartRepository;
 import com.codewithmosh.store.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.Webhook;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
 
@@ -20,7 +26,12 @@ public class CheckoutService {
     private final OrderRepository orderRepository;
     private final AuthService authService;
     private final CartService cartService;
+    private final PaymentGateway paymentGateway;
 
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Transactional
     public CheckoutResponse checkout(CheckoutRequest request) {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if(cart == null) {
@@ -35,9 +46,30 @@ public class CheckoutService {
 
         orderRepository.save(order);
 
-        cartService.clearCart(cart.getId());
+        try {
+            var session = paymentGateway.createCheckoutSession(order);
 
-        return new CheckoutResponse(order.getId());
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(), session.getCheckoutUrl());
+        } catch (PaymentException e) {
+            orderRepository.delete(order);
+            throw e;
+        }
+
+    }
+
+    public void handleWebhookEvent(WebhookRequest request) {
+        paymentGateway
+                .parseWebhookRequest(request)
+                .ifPresent(paymentResult -> {
+                    var order = orderRepository
+                            .findById(paymentResult.getOrderId())
+                            .orElseThrow();
+                    order.setStatus(paymentResult.getPaymentStatus());
+                    orderRepository.save(order);
+                });
+
 
     }
 }
